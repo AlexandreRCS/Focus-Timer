a26e5eec1ad62bedbf04c3b7450608e030c241f2
AlexandreRCS <ale.maresias@gmail.com>
2023-11-20 20:56:09 -0300

    js
    inicio da função countdonw

--- scr/focus-timer/timer.js
+++ scr/focus-timer/timer.js
@@ -2,6 +2,16 @@ import  state from "./state.js";
 import * as ele from "./elements.js"
 
 
+export function countdown(){
+    if(!state.isRunning){
+        return
+    }
+
+    console.log('iniciou')
+    setTimeout(()=> countdown(), 1000)
+}
+
+
 export function updateScreen(minutes, seconds){
     minutes = minutes ?? state.minutes
     seconds = seconds ?? state.seconds
