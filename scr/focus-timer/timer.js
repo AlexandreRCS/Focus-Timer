c28d331b3ae3262ac105aadd5db7ee1d4ca64fc1
AlexandreRCS <ale.maresias@gmail.com>
2023-11-19 22:05:56 -0300

    js
    adicionado a logica update display

---
+++ scr/focus-timer/timer.js
@@ -0,0 +1,12 @@
+import  state from "./state.js";
+import * as ele from "./elements.js"
+
+
+export function updateScreen(minutes, seconds){
+    minutes = minutes ?? state.minutes
+    seconds = seconds ?? state.seconds
+
+    ele.minutes.textContent = String(minutes).padStart(2, "0")
+    ele.seconds.textContent = String(seconds).padStart(2, "0")
+
+}
