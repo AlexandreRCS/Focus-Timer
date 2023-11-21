f69c4e760556b43a278b23dc9932a23e594e6a97
AlexandreRCS <ale.maresias@gmail.com>
2023-11-20 22:34:44 -0300

    Update events.js

--- scr/focus-timer/events.js
+++ scr/focus-timer/events.js
@@ -1,6 +1,8 @@
+import state from "./state.js";
 import { controls } from "./elements.js";
 import * as actions from "./actions.js"
 import * as ele from "./elements.js"
+import { updateScreen } from "./timer.js";
 
 export function registerControls(){
     controls.addEventListener('click', (event) => {
@@ -20,4 +22,17 @@ export function setMinutes(){
     })
 
     ele.minutes.onkeypress = (event) => /\d/.test(event.key)
+
+    ele.minutes.addEventListener('blur', (event) =>{
+        let timer = ele.minutes.textContent
+        timer = timer > 60 ? 60 : timer
+
+        state.minutes = timer
+        state.seconds = 0
+
+        updateScreen()
+
+        ele.minutes.removeAttribute('contenteditable')
+
+    })
 }
